c8c842d04550c8944fbd4e5dcae124a4eb51ebc6
CrispyChicken01 <nabilzaman24@gmail.com>
2025-11-30 21:05:34 +0600

    add basic logic

---
+++ tray.py
@@ -0,0 +1,26 @@
+import pystray
+from PIL import Image
+
+
+image = Image.open('Barb_pic.jpg')
+
+
+def do_stuff(icon, item):
+	if str(item) == 'On':
+		print('It is on')
+	elif str(item) == 'Exit':
+		print('-----Exiting-----')
+		icon.stop()
+	else:
+		print('It is off')
+
+icon = pystray.Icon("copy-script", image, menu= pystray.Menu( 
+	pystray.MenuItem('On', do_stuff),
+	pystray.MenuItem('Off', do_stuff),
+	pystray.MenuItem('Exit', do_stuff)
+))
+
+icon.run()
+
+
+
