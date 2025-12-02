b2552fa4a010f3907f052fba47dc38bba029d33b
CrispyChicken01 <nabilzaman24@gmail.com>
2025-12-02 14:56:33 +0600

    Minor

--- tray.py
+++ tray.py
@@ -1,8 +1,8 @@
 import pystray
 from PIL import Image
+import drives_info
 
-
-image = Image.open('Barb_pic.jpg')
+image = Image.open('pic.jpg')
 
 
 def do_stuff(icon, item):
@@ -24,7 +24,7 @@ def do_stuff(icon, item):
 
 icon = pystray.Icon("copy-script", image, menu= pystray.Menu( 
 	pystray.MenuItem('On', do_stuff),
-	pystray.MenuItem('Off', do_stuff),
+	pystray.MenuItem('Off', drives_info.stop),
 	pystray.MenuItem('Submenu', pystray.Menu(
 		pystray.MenuItem('Submenu 1', do_stuff),
 		pystray.MenuItem('Submenu 2', do_stuff)
